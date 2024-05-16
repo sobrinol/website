3825017443e1f1eb7f2a4456d0de815dbbcdc5e9
sobrinol <sobrinol1@montclair.edu>
2024-05-16 12:24:33 -0400

    added new styling to projects, updated resume

--- src/components/About.js
+++ src/components/About.js
@@ -4,24 +4,24 @@ import coding from "./images/coding.jfif";
 import resume from "./files/resume.pdf";
 
 const About = () => {
-    return (
-        <div className = 'about'>
-            <div className = 'content'>
-                <img className = "coding" src = {coding} alt = "coding"></img>
-                <div className = "sec2">
-                    <h2>About</h2>
-                    <p>I am a student from Harrison, NJ studying Computer Science at <a href = "https://www.montclair.edu/academics/computer-science/" className = "msu" target = "_blank">Montclair State University!</a></p>
-                    <p>I am also the captain for the Valorant varsity team at Montclair State University</p>
-                    <br></br>
-                    <p>As a kid, my father owned a computer cafe; maintaining computers and
-                    editing time-managing software was how I was introduced to software and computer science</p>
-                    <br></br>
-                    <p>Ever since, a child's imagination and sense to engineer has never stopped running</p>
-                    <a href= {resume} className = "resume-button" target = "_blank" rel="noreferrer">View My Resume</a>
-                </div>
-            </div>
+  return (
+    <div className='about'>
+      <div className='content'>
+        <img className="coding" src={coding} alt="coding"></img>
+        <div className="sec2">
+          <h2>About</h2>
+          <p>I am a student from Harrison, NJ studying Computer Science at <a href="https://www.montclair.edu/academics/computer-science/" className="msu" target="_blank" rel="noreferrer">Montclair State University!</a></p>
+          <p>I am also the captain for the Valorant varsity team at Montclair State University</p>
+          <br></br>
+          <p>As a kid, my father owned a computer cafe; maintaining computers and
+            editing time-managing software was how I was introduced to software and computer science</p>
+          <br></br>
+          <p>Ever since, a child's imagination and sense to engineer has never stopped running</p>
+          <a href={resume} className="resume-button" target="_blank" rel="noreferrer">View My Resume</a>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,58 +4,86 @@ import websiteport from "./images/websiteport.png";
 import montclarionport from "./images/montclarionport.png";
 
 const Projects = () => {
-    return(
-        <div className = "projects">
-            <div className = "project-content">
-            <h2 className = "proj-title">Projects</h2> 
-                <div className = "banners-grid">
-                <div className = "banner">
-                    <a className = "project-links" href = "https://github.com/sobrinol/portfolio-website" target = "_blank">
-                    <img src = {websiteport}>
-                    </img>
-                    
-                    <span className = "line"></span>
-                    <h2>Website Portfolio</h2>
-                    </a>
-                    <ul>
-                        <li>
-                            Utilized ReactJS and its libraries to make a single page portfolio website
-                        </li>
-                        <li>
-                            Broadened knowledge in web design with ReactJS, Git, NodeJS, and JavaScript
-                        </li>
-                        <li>
-                            Accomplished my first project, I would still love to add more to this site
-                        </li>
-                    </ul>
-                </div>
-                <div className = "banner">
-                    <a className = "project-links" href = "https://themontclarion.org/" target = "_blank">
-                        <img src = {montclarionport}></img>
-                    
-                    <h2>The Montclarion Website</h2>
-                    </a>
-                    <ul>
-                        <li>
-                            Broadened knowledge of website building by using Wordpress to re-build and re-design the Montclarion's website
-                        </li>
-                        <li>
-                            Obtained experience in SFTP and site data migration by migrating over 32GB of post data from the old page to the redesigned one
-                        </li>
-                        <li>
-                            Learnt about SEO optimization and expanded design knowledge
-                        </li>
-                        <li>
-                            Accomplished first professional project
-                        </li>
-                    </ul>
-                </div>
-                
-                </div>
-            </div>
-            
+  return (
+    <div className="projects">
+      <div className="project-content">
+        <h2 className="proj-title">Projects</h2>
+        <div className="banners-grid">
+          <div className="banner">
+            <a className="project-links" href="https://themontclarion.org/" target="_blank" rel="noreferrer">
+              <img src={montclarionport} alt="The Montclarion Website" />
+              <h2>The Montclarion Website</h2>
+            </a>
+            <ul>
+              <li>
+                Broadened knowledge of website building by using Wordpress to re-build and re-design the Montclarion's website
+              </li>
+              <li>
+                Obtained experience in SFTP and site data migration by migrating over 32GB of post data from the old page to the redesigned one
+              </li>
+              <li>
+                Learnt about SEO optimization and expanded design knowledge
+              </li>
+            </ul>
+          </div>
+          <div className="banner">
+            <a className="project-links" href="https://github.com/sobrinol/portfolio-website" target="_blank" rel="noreferrer">
+              <img src={websiteport} alt="Website Portfolio" />
+              <span className="line"></span>
+              <h2>SOMMA Music Player</h2>
+            </a>
+            <ul>
+              <li>
+                Utilized ReactJS and its libraries to make a single page portfolio website
+              </li>
+              <li>
+                Broadened knowledge in web design with ReactJS, Git, NodeJS, and JavaScript
+              </li>
+              <li>
+                Accomplished my first project, I would still love to add more to this site
+              </li>
+            </ul>
+          </div>
+          <div className="banner">
+            <a className="project-links" href="https://github.com/sobrinol/portfolio-website" target="_blank" rel="noreferrer">
+              <img src={websiteport} alt="Website Portfolio" />
+              <span className="line"></span>
+              <h2>Website Portfolio</h2>
+            </a>
+            <ul>
+              <li>
+                Utilized ReactJS and its libraries to make a single page portfolio website
+              </li>
+              <li>
+                Broadened knowledge in web design with ReactJS, Git, NodeJS, and JavaScript
+              </li>
+              <li>
+                Accomplished my first project, I would still love to add more to this site
+              </li>
+            </ul>
+          </div>
+          <div className="banner">
+            <a className="project-links" href="https://github.com/sobrinol/portfolio-website" target="_blank" rel="noreferrer">
+              <img src={websiteport} alt="Website Portfolio" />
+              <span className="line"></span>
+              <h2>Website Portfolio</h2>
+            </a>
+            <ul>
+              <li>
+                Utilized ReactJS and its libraries to make a single page portfolio website
+              </li>
+              <li>
+                Broadened knowledge in web design with ReactJS, Git, NodeJS, and JavaScript
+              </li>
+              <li>
+                Accomplished my first project, I would still love to add more to this site
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
